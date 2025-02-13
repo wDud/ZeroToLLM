ce382b3b29d8869ea19c3b30159ba948a34ba7e2
WDud <1434609625@qq.com>
2025-02-13 15:47:48 +0800

    achieve the BPE algorithm with python, include train、encode、decode function

--- TinyLLM/tokenizer.py
+++ TinyLLM/tokenizer.py
@@ -16,89 +16,131 @@ from collections import defaultdict
 
 
 class PyBPE():
-    def __init__(self, corpus, vocab_max_size):
-        self.corpus = corpus
-        self.vocab_max_size = vocab_max_size
+    def __init__(self, vocab_size=10000):
+        self.vocab_size = vocab_size
         self.vocab = defaultdict(int)
-        self.pairs = defaultdict(int)
+        self.merges = {}
 
-    def __init_corpus(self, mode='spacewhite_split'):
-        """
-        初始化：从每个单词的字符级分割开始，初始词汇表包含所有可能的字符
-        Args:
-            mode: 分割方式，英文使用空格，中文使用jieba分词
-
-        Returns:
-
-        """
-        if mode == 'spacewhite_split':
-            words = self.corpus.split()
-        else:
-            words = jieba.lcut(self.corpus)
+    def __process(self, text):
+        words = text.split()
+        vocab = []
         for word in words:
             word = f"<{word}>"
-            symbols = list(word)
-            key = ' '.join(symbols)
-            self.vocab[key] += 1
+            vocab.append(list(word))
+        return vocab
 
-    def __stistic_freq(self):
+    def __init_corpus(self, words):
+        """
+        初始化：从每个单词的字符级分割开始，初始词汇表包含所有可能的字符
+        Returns:
+        """
+        for word in words:
+            for char in word:
+                self.vocab[char] += 1
+
+    def __get_stats(self, words):
         """
         统计频率：对语料中所有字符对的出现频率进行统计
         """
         pairs = defaultdict(int)
-        for word in self.vocab:
-            symbols = word.split()
-            if len(symbols) == 1:
-                pairs[symbols[0]] += 1
-                continue
+        for word in words:
+            symbols = word
             for i in range(len(symbols) - 1):
-                pair = (symbols[i], symbols[i + 1])
-                pairs[pair] += 1
+                pairs[(symbols[i], symbols[i+1])] += 1
+        return pairs
 
-        self.pairs = pairs
-
-
-    def __merge_pair(self):
+    def __merge_vocab(self, pair, words):
         """
         合并操作：找到最频繁的字符对，将其合并为一个新的子词，并更新词汇表
         Returns:
 
         """
-        while True:
-            high_freq_pair = max(self.pairs, key=self.pairs.get)
-            if self.pairs[high_freq_pair] == 1:
-                break
-            replace_word = high_freq_pair[0] + high_freq_pair[1]
-            replace_target = " ".join(list(high_freq_pair))
-            vocab = defaultdict(int)
-            pairs = copy.deepcopy(self.pairs)
-            for word in self.vocab:
-                if replace_target in word:
-                    word = word.replace(replace_target, replace_word)
-                vocab[word] += 1
-            self.vocab = vocab
-            self.__stistic_freq()
+        replace_word = ''.join(list(pair))
+        replace_target = ' '.join(list(pair))
 
-    def bpe_segment(self, num_merges=5):
+        new_words = []
+        for word in words:
+            word = ' '.join(word)
+            if replace_target in word:
+                word = word.replace(replace_target, replace_word)
+            new_words.append(word.split(' '))
+        return new_words
+
+    def train(self, corpus):
         """
         bpe分词算法
         """
-        self.__init_corpus()
-        self.__stistic_freq()
-        self.__merge_pair()
+        # 初始化词汇表（字符级）
+        words = self.__process(corpus)
+        self.__init_corpus(words)
 
-        vocab = []
-        for symbol in self.vocab:
-            words = symbol[1: -1].split(" ")
-            vocab.extend(words)
-        self.vocab = [_ for _ in set(vocab) if _]
 
+        num_merges = self.vocab_size - len(self.vocab)
+        for i in range(num_merges):
+            # 获取字符对
+            pairs = self.__get_stats(words)
+            if not pairs:
+                break
+            # 每次选择一个出现频率最大的进行合并
+            best_pair = max(pairs, key=pairs.get)
+            words = self.__merge_vocab(best_pair, words)
+
+            # 记录merge合并的顺序，越小，合并优先级越高
+            self.merges[best_pair] = i
+
+            # 将新合并的符号加入词汇表
+            merge_word = ''.join(best_pair)
+            self.vocab[merge_word] += pairs[best_pair]
+        return self.merges
+
+    def encode(self, text):
+        """
+        编码，同样，拆分成字符级，然后不断地进行合并，得到最优拆分粒度
+        Args:
+            text: 编码文本
+
+        Returns:
+            分词结果
+        """
+
+        words = self.__process(text)
+        tokens = []
+        for word in words:
+            while len(word) > 1:
+                pairs = self.__get_stats([word])
+                if not pairs:
+                    break
+
+                best_pair = min(pairs.keys(), key=lambda pair: self.merges.get(pair, float('inf')))
+
+                if best_pair not in self.merges:
+                    break
+                word = self.__merge_vocab(best_pair, [word])[0]
+            tokens.extend(word)
+        return tokens
+
+    def decode(self, tokens):
+        """
+        解码，还原文本，根据单词起始、终止边界来进行还原
+        Args:
+            tokens: 分词结果
+
+        Returns:
+            还原文本
+        """
+        text = ''.join(tokens)
+        text = text.replace('><', ' ').replace('<', '').replace('>', ' ')
+        return text
 
 
 if __name__ == '__main__':
+    # BPE只是一种分词算法，需要进一步映射词表，处理oov，才可以用于大模型训练
     # corpus = "hello world, how are you? nice to meet you, good morning, do you have a work?"
-    corpus = "hug pug pun bun hugs"
+    corpus = "apple apply ape banana"
     vocab_max_size = 50
-    bpe = PyBPE(corpus, vocab_max_size)
-    bpe.bpe_segment()
-    print(bpe.vocab)
+    bpe = PyBPE()
+    merges = bpe.train(corpus)
+    text = "app aple anban abp ly ana apple banan"
+    tokens = bpe.encode(text)
+    print(tokens)
+    print(bpe.decode(tokens))
